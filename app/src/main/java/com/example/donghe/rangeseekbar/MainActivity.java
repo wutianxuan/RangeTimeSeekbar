a24e343ce08d25b05e39e5c1c5e59705173e2c79
tianer <1055480407@qq.com>
2020-06-30 23:24:24 +0800

    切换时间

--- app/src/main/java/com/example/donghe/rangeseekbar/MainActivity.java
+++ app/src/main/java/com/example/donghe/rangeseekbar/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv_right)
     TextView tvRight;
     //String arrays[] = {"0", "5", "10", "15", "20", "25", "40", "60", "80", "100"};
-    String[]  arrays = {"08:00", "08:30", "09:00", "10:00", "10:30", "11:00", "11:30", "12:00"};
+    String[]  arrays = {"08:00", "08:30", "09:00", "09:30","10:00", "10:30", "11:00", "11:30", "12:00"};
+    String[]  array2 = {"14:00", "14:30",  "15:00","15:30", "16:00", "16:30", "17:00", "17:30", "18:00"};
+    String[]  arrays3 = {"19:00", "19:30","20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
