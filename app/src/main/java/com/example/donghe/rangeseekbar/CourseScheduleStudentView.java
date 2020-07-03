fe92ccd96342236ab4676fbc91543d3c8bddda88
tianer <1055480407@qq.com>
2020-07-03 18:05:47 +0800

    加入自定义管理组件

--- app/src/main/java/com/example/donghe/rangeseekbar/CourseScheduleStudentView.java
+++ app/src/main/java/com/example/donghe/rangeseekbar/CourseScheduleStudentView.java
@@ -1,10 +1,13 @@
 package com.example.donghe.rangeseekbar;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
-import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,9 +17,10 @@ import android.widget.TextView;
  */
 class CourseScheduleStudentView extends RelativeLayout {
 
-    private TextView tvTitleTime = new TextView(getContext());
-    private TextView tvTitleCourse = new TextView(getContext());
-    private TextView tvTitleStudent = new TextView(getContext());
+    private TextView tvTitleTime ;
+    private TextView tvTitleCourse ;
+    private TextView tvTitleStudent;
+    private Context mContext;
 
 
     public CourseScheduleStudentView(Context context) {
@@ -29,16 +33,28 @@ class CourseScheduleStudentView extends RelativeLayout {
 
     public CourseScheduleStudentView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mContext = context;
+        TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.CourseScheduleStudentView);
+        typedArray.recycle();
         initView();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        super.onLayout(changed, l, t, r, b);
     }
 
     private void initView() {
+        View layoutInflater = LayoutInflater.from(mContext).inflate(R.layout.activity_test, null);
+        tvTitleTime = new TextView(mContext);
+        tvTitleCourse = new TextView(mContext);
+        tvTitleStudent = new TextView(mContext);
         tvTitleTime.setText("时间");
         tvTitleTime.setHeight(64);
         tvTitleTime.setTextColor(Color.parseColor("#333333"));
@@ -51,6 +67,15 @@ class CourseScheduleStudentView extends RelativeLayout {
         tvTitleStudent.setHeight(64);
         tvTitleStudent.setTextColor(Color.parseColor("#333333"));
         tvTitleStudent.setGravity(Gravity.CENTER);
+
+        addView(tvTitleTime);
+        addView(tvTitleCourse);
+        addView(tvTitleStudent);
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
 }
