422fa84936fb4687d5ebd80b2a49168057f555df
tianer <1055480407@qq.com>
2020-07-08 18:16:22 +0800

    加入时间

--- app/src/main/java/com/example/donghe/rangeseekbar/CourseScheduleStudentView.java
+++ app/src/main/java/com/example/donghe/rangeseekbar/CourseScheduleStudentView.java
@@ -18,9 +18,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
- * Created by Tian on 2020/7/2.
+ *
+ * @author Tian
+ * @date 2020/7/2
  */
-class CourseScheduleStudentView extends RelativeLayout {
+public class CourseScheduleStudentView extends RelativeLayout {
     private static final String TAG = "CourseScheduleStudentVi";
 
     private TextView tvTitleTime;
@@ -34,6 +36,11 @@ class CourseScheduleStudentView extends RelativeLayout {
     private Rect mCourse;
     private Paint mCoursePaint;
     private float mDensity;
+    private String[] arrays = {"08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00"};
+    private String[] array2 = {"14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"};
+    private String[] arrays3 = {"19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00"};
+    private LinearLayout linearTime;
+    private LinearLayout ItemLinearTime;
 
 
     public CourseScheduleStudentView(Context context) {
@@ -55,22 +62,57 @@ class CourseScheduleStudentView extends RelativeLayout {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
-        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
-        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
 
-        Log.e(TAG, sizeWidth + "," + sizeHeight);
-        //1获取当前控件的大小
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
+        // 获取宽-测量规则的模式和大小
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
 
+        // 获取高-测量规则的模式和大小
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        Log.e(TAG, "widthSize:"+widthSize + ","+"heightSize:" + heightSize);
+        Log.e(TAG, "widthMode:"+widthMode + ","+"heightMode:" + heightMode);
+
+        int layoutWidth = 0;
+        int layoutHeight = 0;
+        // 计算出所有的childView的宽和高
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        int cWidth = 0;
+        int cHeight = 0;
+        int count = getChildCount();
+
+        if(widthMode == MeasureSpec. EXACTLY){
+            //如果布局容器的宽度模式是确定的（具体的size或者match_parent），直接使用父窗体建议的宽度
+            layoutWidth = widthSize;
+        } else{
+            //如果是未指定或者wrap_content，我们都按照包裹内容做，宽度方向上只需要拿到所有子控件中宽度做大的作为布局宽度
+            for ( int i = 0; i < count; i++)  {
+                View child = getChildAt(i);
+                cWidth = child.getMeasuredWidth();
+                //获取子控件最大宽度
+                layoutWidth = Math.max(cWidth, layoutWidth);
+            }
+        }
+        //高度很宽度处理思想一样
+        if(heightMode == MeasureSpec. EXACTLY){
+            layoutHeight = heightSize;
+        } else{
+            for ( int i = 0; i < count; i++)  {
+                View child = getChildAt(i);
+                cHeight = child.getMeasuredHeight();
+                layoutHeight = Math.max(cHeight, layoutHeight);
+            }
+        }
+        Log.e(TAG, "layoutWidth:"+layoutWidth + ","+"layoutHeight:" + layoutHeight);
         //获取所有子控件，拿到它的layoutparams对象，这样就可以拿到他的自定义属性
         int childCount = this.getChildCount();
-        int viewWidth = width/3;
+        int viewWidth = layoutWidth/3;
         int usedWidth = 0 ;
         for (int i = 0; i < childCount; i++) {
             View childAt = getChildAt(i);
+            //自view自己测量
+            //measureChild(childAt, widthMeasureSpec, heightMeasureSpec);
             MarginLayoutParams layoutParams = (MarginLayoutParams) childAt.getLayoutParams();
             switch (i) {
                 case 0:
@@ -78,15 +120,16 @@ class CourseScheduleStudentView extends RelativeLayout {
 
                     break;
                 case 1:
-                    layoutParams.width = (width-usedWidth-layoutParams.leftMargin-layoutParams.rightMargin)/2;
+                    layoutParams.width = (layoutWidth-usedWidth-layoutParams.leftMargin-layoutParams.rightMargin)/2;
                     break;
                 case 2:
-                    layoutParams.width = width-usedWidth;
+                    layoutParams.width = layoutWidth-usedWidth;
                     break;
             }
             usedWidth += layoutParams.width;
+            // 测量并保存layout的宽高
+            setMeasuredDimension(layoutWidth, layoutHeight);
         }
-
     }
 
 
@@ -171,6 +214,22 @@ class CourseScheduleStudentView extends RelativeLayout {
         tvTitleStudent.setBackgroundColor(Color.parseColor("#5D91BF"));
         tvTitleStudent.setTextColor(Color.parseColor("#333333"));
         tvTitleStudent.setGravity(Gravity.CENTER);
+        linearTime = new LinearLayout(mContext);
+        linearTime.setOrientation(LinearLayout.VERTICAL);
+        for (int i = 0; i < arrays.length; i++) {
+            ItemLinearTime = new LinearLayout(mContext);
+            ItemLinearTime.setOrientation(LinearLayout.HORIZONTAL);
+            ViewGroup.LayoutParams layoutParams = ItemLinearTime.getLayoutParams();
+            layoutParams.height = 200;
+            layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+            /*layoutParams.
+            ItemLinearTime.setLayoutParams(
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200));*/
+            TextView timeTV = new TextView(mContext);
+            timeTV.setText(arrays[i]);
+        }
+
+
         addView(tvTitleTime);
         addView(tvTitleCourse);
         addView(tvTitleStudent);
@@ -185,37 +244,14 @@ class CourseScheduleStudentView extends RelativeLayout {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //mCoursePaint.setColor(Color.parseColor("#7DCECD"));
-        //canvas.drawRect(mCourse, mCoursePaint);
+        mCoursePaint.setColor(Color.parseColor("#7DCECD"));
+        canvas.drawRect(mCourse, mCoursePaint);
 
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
-        int count = getChildCount();
-        for (int i = 0; i < count; i++) {
-
-            View childAt = getChildAt(i);
-            switch (i) {
-                case 0:
-                    mCourse.left = childAt.getLeft();
-                    mCourse.right = childAt.getRight();
-                    mCourse.top = textTitleDivider+childAt.getBottom();
-                    mCourse.bottom = 500+mCourse.top;
-                    mCoursePaint.setColor(Color.RED);
-                    canvas.drawRect(mCourse, mCoursePaint);
-                    //canvas.drawLine( 200,40,200,100,mCoursePaint);
-                    break;
-                case 1:
-                    //layoutParams.width = (width-usedWidth-layoutParams.leftMargin-layoutParams.rightMargin)/2;
-                    break;
-                case 2:
-                    //layoutParams.width = width-usedWidth;
-                    break;
-            }
-
-        }
 
     }
 
@@ -233,6 +269,7 @@ class CourseScheduleStudentView extends RelativeLayout {
     public RelativeLayout.LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new RelativeLayout.LayoutParams(getContext(),attrs);
     }*/
+
     private void initPaint() {
         mCoursePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mCoursePaint.setAntiAlias(true);
